8f4b66721e1316b3861652de82b8fa9c331bf831
rafabrazil <rafael.h@outlook.com>
2022-07-11 14:09:32 -0300

    Páginas e rotas - Layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 
 import { GlobalStyled } from './style/global'
 import { defaultTheme } from './style/themes/default'
@@ -6,7 +8,9 @@ import { defaultTheme } from './style/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>oi</div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyled />
     </ThemeProvider>
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import logoIgnite from '../assets/logo-ignite.svg'
+export function Header() {
+  return (
+    <header>
+      <img src={logoIgnite} alt="Logotipo" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
